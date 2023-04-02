72e447effd5158a3352fc47d681afc9de9f6c1c9
Rakesh Giri <rakeshgiri.korba@gmail.com>
2023-04-03 00:36:29 +0530

    Add files via upload

---
+++ components/features/posts/counterAPI.ts
@@ -0,0 +1,17 @@
+
+import { resolve } from "path";
+
+// A mock function to mimic making an async request for data
+// export function fetchCount(amount = 1) {
+//   return new Promise<{ data: number }>((resolve) =>
+//     setTimeout(() => resolve({ data: amount }), 500)
+//   );
+// }
+
+
+
+// export function fatchData(Data = fetch('https://jsonplaceholder.typicode.com/posts')) {
+//   return new Promise<{ data: string }>((resolve)=> 
+//   setTimeout(() => resolve({ data : null}), 500)
+//   );  
+// }
